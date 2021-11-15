627fe62a014321b8ee1a26567097778a33a14941
VolodymyrBushko <volodymyr.bushko@gmail.com>
2021-11-15 19:38:41 +0200

    Initialization

---
+++ src/main/java/org/vbushko/skylon/SkylonApplication.java
@@ -0,0 +1,12 @@
+package org.vbushko.skylon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkylonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkylonApplication.class, args);
+    }
+}
---
+++ src/test/java/org/vbushko/skylon/SkylonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vbushko.skylon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkylonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
