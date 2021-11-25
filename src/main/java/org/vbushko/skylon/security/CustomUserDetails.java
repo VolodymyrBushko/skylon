1ba0947ec635dc8f1265ed6d910cddb259a50e47
VolodymyrBushko <volodymyr.bushko@gmail.com>
2021-11-25 23:48:33 +0200

    Add UserDetails and UserDetailsService

---
+++ src/main/java/org/vbushko/skylon/security/CustomUserDetails.java
@@ -0,0 +1,54 @@
+package org.vbushko.skylon.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private String login;
+    private String password;
+    private Collection<? extends GrantedAuthority> grantedAuthorities;
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return grantedAuthorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/org/vbushko/skylon/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package org.vbushko.skylon.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.vbushko.skylon.user.entity.User;
+import org.vbushko.skylon.user.service.UserService;
+
+import java.util.Collections;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserService service;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = service.findByLogin(username);
+        return CustomUserDetails.builder()
+                .login(user.getLogin())
+                .password(user.getPassword())
+                .grantedAuthorities(Collections.singleton(new SimpleGrantedAuthority("USER")))
+                .build();
+    }
+}
